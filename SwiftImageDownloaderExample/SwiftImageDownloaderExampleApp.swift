e00bdbb4c1c70ebe9f84d31f1159c7bcf01970dc
Ratnesh <ratneshjain1993@gmail.com>
2025-01-18 17:20:15 +0530

    Update to Swift-Dependencies.

--- SwiftImageDownloaderExample/SwiftImageDownloaderExampleApp.swift
+++ SwiftImageDownloaderExample/SwiftImageDownloaderExampleApp.swift
@@ -5,10 +5,20 @@
 //  Created by Ratnesh Jain on 18/01/25.
 //
 
+import Dependencies
 import SwiftUI
 
 @main
 struct SwiftImageDownloaderExampleApp: App {
+    
+    init() {
+        prepareDependencies {
+            $0.colorCacheConfig.allowCache = { false }
+            $0.imageCacheConfig.totalCostLimit = { 10 }
+            $0.storageClient.imageCachePath = { URL.temporaryDirectory.appending(path: "Images") }
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
